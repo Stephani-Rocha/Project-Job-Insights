29afff930cf808cd452c213583b534052901d6f8
Stephani Rocha <gomes.stephani19@gmail.com>
2022-12-06 17:35:05 -0300

    feat(get_unique_industries): Implementa a função get_unique_industries que retorna uma lista de industrias

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,13 @@
 from typing import List, Dict
-
+from .jobs import read
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    jobs_reader = read(path)
+    jobs_industry = set()
+    for job in jobs_reader:
+        if len(job["industry"]) > 0:
+            jobs_industry.add(job["industry"])
+    return jobs_industry
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
