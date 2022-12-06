e283a6ebd0e4d4c34828f1785b830d6e97a768da
Stephani Rocha <gomes.stephani19@gmail.com>
2022-12-06 16:48:59 -0300

    feat(read e get_unique_job_types): Implementa a função read, que faz a leitura do arquivo csv e implementa a função get_unique_job_types, que retorna uma lista com todos os jobs types.

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,40 +1,22 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    with open(path, encoding="utf-8") as file:
+        jobs_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        return list(jobs_reader)
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
+    jobs_reader = read(path)
+    jobs = set()
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    for job in jobs_reader:
+        jobs.add(job["job_type"])
+    return jobs
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
